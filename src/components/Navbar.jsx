2f4416dcd13f8ca0a2289bdae4b1c52160c91557
kavin1231 <chamathkaveendra@gmail.com>
2025-05-05 13:05:24 +0530

    enhance Navbar component; implement user login/logout functionality and improve mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { assets } from "../assets/assets";
+import { useAppContext } from "./context/AppContext";
 
 const Navbar = () => {
   const [open, setOpen] = React.useState(false);
+  const { user, setUser, setShowUserLogin, navigate } = useAppContext();
+
+
+  const logout=async()=>{
+    setUser(null);
+    navigate("/");
+  }
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
       <NavLink to="/">
@@ -47,7 +55,9 @@ const Navbar = () => {
       </button>
 
       {/* Mobile Menu */}
-      <div
+
+      {open && (
+        <div
         className={`${
           open ? "flex" : "hidden"
         } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
@@ -58,13 +68,31 @@ const Navbar = () => {
         <NavLink to="/products" onClick={() => setOpen(false)}>
           All Product
         </NavLink>
+        {user && (
+          <NavLink to="/" onClick={() => setOpen(false)}>
+            My Orders
+          </NavLink>
+        )}
         <NavLink to="/" onClick={() => setOpen(false)}>
-          My Orders
+          Contact
         </NavLink>
-        <button className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
-          Login
-        </button>
+        {!user ? (
+          <button
+            onClick={() => {
+              setOpen(false);
+              setShowUserLogin(true);
+            }}
+            className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm"
+          >
+            Login
+          </button>
+        ) : (
+          <button onClick={logout}className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+            Logout
+          </button>
+        )}
       </div>
+)}
     </nav>
   );
 };
